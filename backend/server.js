91e1d570c44eaed05f398e6bba557ab309a5907a
kishor <kishorpeetani7@gmail.com>
2026-05-01 12:01:29 +0530

    origin corrected

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://to-do-list-iota-hazel-87.vercel.app/",
+    origin: "https://to-do-list-iota-hazel-87.vercel.app",
     credentials: true
 }));
 app.use(cookieParser());
